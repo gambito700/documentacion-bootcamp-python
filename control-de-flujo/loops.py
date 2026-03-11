824409115e058372f26b63c4eafabe56c75c9280
gambito700 <gambitocerati@gmail.com>
2026-03-11 06:15:49 -0300

    docs: agrego ejemplos de loops

---
+++ control-de-flujo/loops.py
@@ -0,0 +1,11 @@
+#primero los "for" para probar ejemplos de loops
+
+for i in range(5):
+"""
+for → es la instrucción que inicia el loop
+i → es el nombre de la variable, como lo nombramos, tienes razón
+in → significa "dentro de". Le dice al for dónde tiene que buscar los elementos. Se lee literalmente: "para cada i dentro de range(5)"
+range(5) → correcto, genera la secuencia de números del 0 al 4
+(5) → correcto, es lo que le indica hasta dónde llegar
+"""
+    print(i)
